7ce1dc098661d4ee04bc9a9b0b1aaecc0f5d8387
sumi4ki <153429945+sumi4ki@users.noreply.github.com>
2023-12-21 21:30:39 +0900

    clampx
    よこだけ直した

--- Assets/Script/Main/Player.cs
+++ Assets/Script/Main/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
     // マウスドラッグ処理
     private float previousPosX;
     private float currentPosX;
+    
+    // x座標をclampするときの最小値、最大値
+    private float xMin;
+    private float xMax;
 
     // player size
     private float radius;
@@ -39,14 +43,36 @@ public class Player : MonoBehaviour
             currentPosX = Input.mousePosition.x;
             // 移動距離の計算　Screen.widthで割って1に正規化. 定数をかけて,マウスの移動とplayerが一致
             float diffDistance = (currentPosX - previousPosX) / Screen.width * displayWidth*2;
-            // 次のローカルx座標を設定
+
+            // 次のローカルx座標を設定  CLAMP
+            // 左右にcast a ray
+            
+            RaycastHit2D L = Physics2D.Raycast(transform.position+new Vector3(-radius,0f,0f), Vector2.left);
+            RaycastHit2D R = Physics2D.Raycast(transform.position+new Vector3(radius,0f,0f), Vector2.right);
+            
+            // 左にコライダーがあるとき
+            if(L.collider != null) {
+                xMin = L.transform.position.x + L.transform.localScale.x / 2 + radius;
+                
+            } else {
+                xMin = -displayWidth + radius;
+            }
+            // 右にコライダーがあるとき
+            if(R.collider != null) {
+                xMax = R.transform.position.x - R.transform.localScale.x / 2 - radius;
+            } else {
+                xMax = displayWidth - radius;
+            }
+
             Vector2 pos = transform.position;
-            pos.x = Mathf.Clamp(pos.x + diffDistance,  -displayWidth + radius,  displayWidth - radius);
+            pos.x = Mathf.Clamp(pos.x + diffDistance,  xMin,  xMax);
+            
             transform.position = pos;
+            
 
             // タップ位置を更新
             previousPosX = currentPosX;
-        }
+        } 
     }
 
 

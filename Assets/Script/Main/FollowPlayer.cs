03afcd99944976e8f19c11107ce5446fb865fe90
sumi4ki <153429945+sumi4ki@users.noreply.github.com>
2023-12-24 13:20:52 +0900

    13:20

--- Assets/Script/Main/FollowPlayer.cs
+++ Assets/Script/Main/FollowPlayer.cs
@@ -6,16 +6,23 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 2, -1);
+
+    // Stageオブジェクトしている
+    [SerializeField] private GameObject Stage;
+    // stagescript
+    private StageScript stageScript;
     
     void Start()
     {
-        
+        // StageScript取得
+        stageScript = Stage.GetComponent<StageScript>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        
+        // gameOverだったらfollowplayerを停止する
+        if(stageScript.IsGameover == true) return;
         // playerのy座標値についていく
         Vector3 pos = transform.position;
         pos.y = player.transform.position.y + offset.y;
--- Assets/Script/Main/Player.cs
+++ Assets/Script/Main/Player.cs
@@ -9,7 +9,12 @@ public class Player : MonoBehaviour
     // スクリプトのインスタンス
     private FollowTransform marker;
     // HPテキストのgameObject
-    [SerializeField]private GameObject HPtext;
+    [SerializeField] private GameObject HPtext;
+    // Stageオブジェクト
+    [SerializeField] private GameObject Stage;
+    // stagescript
+    private StageScript stageScript;
+
 
     // マウスドラッグ処理
     private float previousPosX;
@@ -42,6 +47,8 @@ public class Player : MonoBehaviour
         HP = 100;
         // HPtextのスクリプト取得
         marker = HPtext.GetComponent<FollowTransform>();
+        // StageScript取得
+        stageScript = Stage.GetComponent<StageScript>();
 
         // プレイヤーの半径
         radius = GetComponent<Transform>().transform.localScale.x / 2;
@@ -128,8 +135,10 @@ public class Player : MonoBehaviour
     }
     void OnCollisionStay2D(Collision2D c)
     {
+        // gameOver
         if(HP <= 0) {
             destroyText();
+            stageScript.IsGameover = true;
             Destroy(gameObject);
         }
         /*

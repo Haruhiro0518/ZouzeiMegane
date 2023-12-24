b13e83e452819cf754db4357568106b0100aa94b
Haruhiro0518 <153417008+Haruhiro0518@users.noreply.github.com>
2023-12-24 10:51:01 +0900

    Updated

--- Assets/Script/Intro/IntroManager.cs
+++ Assets/Script/Intro/IntroManager.cs
@@ -10,6 +10,12 @@ public class IntroManager : MonoBehaviour
     
     private bool talking;
 
+    [SerializeField, Header("総理UI")]
+    private GameObject PlayerUI;
+
+    [SerializeField, Header("秘書UI")]
+    private GameObject NonPlayerUI;
+    
     [SerializeField, Header("オプションUI")]
     private GameObject OptionUI;
 
@@ -22,12 +28,41 @@ public class IntroManager : MonoBehaviour
     [SerializeField, Header("秘書アニメーター")]
     Animator NonPlayerAnimator;
 
+    [SerializeField, Header("テキスト1")]
+    private GameObject Text1;
+
+    [SerializeField, Header("テキスト2")]
+    private GameObject Text2;
+
+    [SerializeField, Header("テキスト3")]
+    private GameObject Text3;
+
+    [SerializeField, Header("テキスト4")]
+    private GameObject Text4;
+
+    [SerializeField, Header("テキスト5")]
+    private GameObject Text5;
+
+    [SerializeField, Header("テキスト6")]
+    private GameObject Text6;
+
+    [SerializeField, Header("テキスト7")]
+    private GameObject Text7;
+
+    [SerializeField, Header("テキスト8")]
+    private GameObject Text8;
+
     private AudioSource introBGM;
     
     void Start()
     {
         state = 0;
-        talking = true;
+        talking = false;
+        PlayerUI.SetActive(talking);
+        NonPlayerUI.SetActive(!talking);
+        PlayerAnimator.SetBool("talking", talking);
+        NonPlayerAnimator.SetBool("talking", !talking);
+        Text1.SetActive(true);
         introBGM = GetComponent<AudioSource>();
         introBGM.volume = TitleManager.volumeValue;
         introBGM.Play();
@@ -48,14 +83,62 @@ public class IntroManager : MonoBehaviour
         state += 1;
         talking = !talking;
 
-        if(state > 3)
+        if(state > 7)
         {
             SceneManager.LoadScene("Main");
         }
         else
         {
+            PlayerUI.SetActive(talking);
+            NonPlayerUI.SetActive(!talking);
             PlayerAnimator.SetBool("talking", talking);
             NonPlayerAnimator.SetBool("talking", !talking);
+
+            switch(state)
+            {
+                case 1:
+                {
+                    Text1.SetActive(false);
+                    Text2.SetActive(true);
+                    break;
+                }
+                case 2:
+                {
+                    Text2.SetActive(false);
+                    Text3.SetActive(true);
+                    break;
+                }
+                case 3:
+                {
+                    Text3.SetActive(false);
+                    Text4.SetActive(true);
+                    break;
+                }
+                case 4:
+                {
+                    Text4.SetActive(false);
+                    Text5.SetActive(true);
+                    break;
+                }
+                case 5:
+                {
+                    Text5.SetActive(false);
+                    Text6.SetActive(true);
+                    break;
+                }
+                case 6:
+                {
+                    Text6.SetActive(false);
+                    Text7.SetActive(true);
+                    break;
+                }
+                case 7:
+                {
+                    Text7.SetActive(false);
+                    Text8.SetActive(true);
+                    break;
+                }
+            }
         }
     }
 
